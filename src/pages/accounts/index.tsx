394851110d4701899aebc75635f985a8c181c09f
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2023-06-13 22:05:17 +0000

    Added minor styling. started working on transactions

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -19,13 +19,14 @@ const AccountList: React.FC = () => {
     const { data: myAccounts } = api.dave.getAllSavingsAccount.useQuery();
     const ctx = api.useContext()
 
-    const { mutate } = api.dave.createSavingsAccount.useMutation({
+    const { mutate, error } = api.dave.createSavingsAccount.useMutation({
       onSuccess: () => {
         void ctx.dave.getAllSavingsAccount.invalidate();
       }
     });
     const someAmount = 800.00;
     const totalSavings = someAmount * (myAccounts ? myAccounts.length : 0);
+    
     return (
       <>
         <p>Total savings<br /><span>${totalSavings}</span></p>
@@ -47,9 +48,11 @@ const AccountList: React.FC = () => {
         ))}
           </tbody>
         </table>
-        <>
+        <div className={styles.container}>
+          <div className={styles.flexOuter}>
           <p>Add account</p>
           <form id="addAccountForm" onSubmit={(e) => {
+
             e.preventDefault();
             const formData = new FormData(e.currentTarget);
             mutate({ 
@@ -60,17 +63,82 @@ const AccountList: React.FC = () => {
             e.currentTarget.reset()
             // TODO: refresh account data 
           }}>
-            <input name="name" type="text" placeholder="Account name"/>
-            <input name="location" type="text" placeholder="Account location" />
-            <select name="type" id="">
-              <option value="BOUND">Bound</option>
-              <option value="UNBOUND">Unbound</option>
-              <option value="BONDS">Bonds</option>
-              <option value="CASH">Cash</option>
-            </select>
+            <ul className={styles.flexOuter}>
+              <li>
+                <input name="name" type="text" placeholder="Account name" />
+                  {error?.data?.zodError?.fieldErrors.name && (
+                    <span className={styles.error}>{error.data.zodError.fieldErrors.name}</span>
+                  )}
+              </li>
+            <li>
+              <input name="location" type="text" placeholder="Account location" />
+              {error?.data?.zodError?.fieldErrors.location && (
+                <span className={styles.error}>{error.data.zodError.fieldErrors.location}</span>
+              )}
+            </li>
+            <li>
+              <select name="type" id="">
+                <option value="BOUND">Bound</option>
+                <option value="UNBOUND">Unbound</option>
+                <option value="BONDS">Bonds</option>
+                <option value="CASH">Cash</option>
+              </select>
+              {error?.data?.zodError?.fieldErrors.type && (
+                <span className={styles.error}>{error.data.zodError.fieldErrors.type}</span>
+              )}
+            </li>
+            <li>
             <button type="submit">Add new account</button>
+            </li>
+            </ul>
           </form>
-        </>
+          </div>
+
+          <div className={styles.flexOuter}>
+          <p>Add transaction</p>
+          <form id="addAccountForm" onSubmit={(e) => {
+
+            e.preventDefault();
+            const formData = new FormData(e.currentTarget);
+            mutate({ 
+              name: formData.get('name'),
+              location: formData.get('location'),
+              type: formData.get('type')
+            })
+            e.currentTarget.reset()
+            // TODO: refresh account data 
+          }}>
+            <ul className={styles.flexOuter}>
+              <li>
+                <input name="name" type="text" placeholder="Account name" />
+                  {error?.data?.zodError?.fieldErrors.name && (
+                    <span className={styles.error}>{error.data.zodError.fieldErrors.name}</span>
+                  )}
+              </li>
+            <li>
+              <input name="location" type="text" placeholder="Account location" />
+              {error?.data?.zodError?.fieldErrors.location && (
+                <span className={styles.error}>{error.data.zodError.fieldErrors.location}</span>
+              )}
+            </li>
+            <li>
+              <select name="type" id="">
+                <option value="BOUND">Bound</option>
+                <option value="UNBOUND">Unbound</option>
+                <option value="BONDS">Bonds</option>
+                <option value="CASH">Cash</option>
+              </select>
+              {error?.data?.zodError?.fieldErrors.type && (
+                <span className={styles.error}>{error.data.zodError.fieldErrors.type}</span>
+              )}
+            </li>
+            <li>
+            <button type="submit">Add new account</button>
+            </li>
+            </ul>
+          </form>
+          </div>
+        </div>
       </>
     )
   }
