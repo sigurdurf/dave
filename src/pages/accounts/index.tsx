b16d64e10c17e4e32732f10b0361fcd90e1c56ef
Sigurður Baldvin Friðriksson <sbf@secureit.is>
2023-06-14 22:45:28 +0000

    Logic,view and schema changes for transactions

--- src/pages/accounts/index.tsx
+++ src/pages/accounts/index.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
-import type { SavingsAccount } from "@prisma/client";
+import type { SavingsAccount, Transaction } from "@prisma/client";
 import { api } from "~/utils/api";
 import styles from "./index.module.css";
-
+import { useEffect, useState } from "react";
+import { useQuery } from 'trpc/client';
 
 const Accounts: NextPage = () => {
 
@@ -14,22 +15,16 @@ const Accounts: NextPage = () => {
 }
 export default Accounts;
 
-const AccountList: React.FC = () => {
-    
-    const { data: myAccounts } = api.dave.getAllSavingsAccount.useQuery();
-    const ctx = api.useContext()
+const Balance: React.FC = ({account}) => {
+  const { data: balance} = api.dave.getSavingsAccountSum.useQuery(account.id)
+  return <span>{balance}</span>
+}
 
-    const { mutate, error } = api.dave.createSavingsAccount.useMutation({
-      onSuccess: () => {
-        void ctx.dave.getAllSavingsAccount.invalidate();
-      }
-    });
-    const someAmount = 800.00;
-    const totalSavings = someAmount * (myAccounts ? myAccounts.length : 0);
-    
-    return (
-      <>
-        <p>Total savings<br /><span>${totalSavings}</span></p>
+const AccountSummary: React.FC = () => {
+  const { data: myAccounts } = api.dave.getAllSavingsAccount.useQuery()
+  return (
+    <>
+    <p>Total savings<br /><span>${0}</span></p>
         <p>Accounts</p>
         <table className={styles.table}>
           <thead>
@@ -39,16 +34,29 @@ const AccountList: React.FC = () => {
           </thead>
           <tbody>
         { myAccounts?.map( (account: SavingsAccount) => (
+          
           <tr className={styles.savingAccountEntry} key={account.id}>
             <td>{account?.name}</td>
             <td>{account?.location}</td>
             <td>{account?.type !== undefined ? account.type : "Some type"}</td>
-            <td>{someAmount}</td>
+            <td><Balance account={account} /></td>
+
           </tr>
         ))}
           </tbody>
         </table>
-        <div className={styles.container}>
+    </>
+  )
+}
+
+const AddAccount: React.FC = () => {
+  const ctx = api.useContext()
+  const { mutate, error } = api.dave.createSavingsAccount.useMutation({
+    onSuccess: () => {
+      void ctx.dave.getAllSavingsAccount.invalidate();
+    }
+  });
+  return (
           <div className={styles.flexOuter}>
           <p>Add account</p>
           <form id="addAccountForm" onSubmit={(e) => {
@@ -93,42 +101,47 @@ const AccountList: React.FC = () => {
             </ul>
           </form>
           </div>
+        
+  )
+}
 
-          <div className={styles.flexOuter}>
+const AddTransaction: React.FC = () => {
+  const ctx = api.useContext()
+  const { mutate, error } = api.dave.createTransaction.useMutation({
+    onSuccess: () => {
+      void ctx.dave.getSavingsAccountSum.invalidate();
+    }
+  });
+  const { data: myAccounts } = api.dave.getAllSavingsAccount.useQuery();
+
+  return (
+    <div className={styles.flexOuter}>
           <p>Add transaction</p>
-          <form id="addAccountForm" onSubmit={(e) => {
+          <form id="addTransactionForm" onSubmit={(e) => {
 
             e.preventDefault();
             const formData = new FormData(e.currentTarget);
             mutate({ 
-              name: formData.get('name'),
-              location: formData.get('location'),
-              type: formData.get('type')
+              amount: parseInt(formData.get('amount')),
+              accountId: formData.get('accountId')
             })
             e.currentTarget.reset()
             // TODO: refresh account data 
           }}>
             <ul className={styles.flexOuter}>
-              <li>
-                <input name="name" type="text" placeholder="Account name" />
-                  {error?.data?.zodError?.fieldErrors.name && (
-                    <span className={styles.error}>{error.data.zodError.fieldErrors.name}</span>
-                  )}
-              </li>
             <li>
-              <input name="location" type="text" placeholder="Account location" />
-              {error?.data?.zodError?.fieldErrors.location && (
-                <span className={styles.error}>{error.data.zodError.fieldErrors.location}</span>
+              <input name="amount" type="text" placeholder="Amount" />
+              {error?.data?.zodError?.fieldErrors.amount && (
+                <span className={styles.error}>{error.data.zodError.fieldErrors.amount}</span>
               )}
             </li>
             <li>
-              <select name="type" id="">
-                <option value="BOUND">Bound</option>
-                <option value="UNBOUND">Unbound</option>
-                <option value="BONDS">Bonds</option>
-                <option value="CASH">Cash</option>
+              <select name="accountId" id="">
+                {myAccounts?.map((account) => (
+                  <option key={account.id} value={account.id}>{account.name}</option>
+                ))}
               </select>
-              {error?.data?.zodError?.fieldErrors.type && (
+              {error?.data?.zodError?.fieldErrors.account && (
                 <span className={styles.error}>{error.data.zodError.fieldErrors.type}</span>
               )}
             </li>
@@ -138,6 +151,20 @@ const AccountList: React.FC = () => {
             </ul>
           </form>
           </div>
+  )
+}
+
+const AccountList: React.FC = () => {
+
+    
+
+    
+    return (
+      <>
+        <AccountSummary/>
+        <div className={styles.container}>
+          <AddAccount />
+          <AddTransaction />
         </div>
       </>
     )
--- src/server/api/routers/dave.ts
+++ src/server/api/routers/dave.ts
@@ -4,6 +4,7 @@ import {
     createTRPCRouter,
     protectedProcedure,
 } from "~/server/api/trpc";
+import { Transaction } from "@prisma/client";
 
 const SavingsAccountType = z.union([
     z.literal('BOUND'),
@@ -11,7 +12,11 @@ const SavingsAccountType = z.union([
     z.literal('BONDS'),
     z.literal('CASH')
 ])
-
+const sum = function(items, prop: string): number {
+    return items.reduce( function(a: number, b: number) : number {
+      return a + b[prop]
+    }, 0)
+  }
 export const daveRouter = createTRPCRouter({
     getAllSavingsAccount: protectedProcedure.query(async ({ ctx }) => {
         const accounts = await ctx.prisma.savingsAccount.findMany({
@@ -42,6 +47,70 @@ export const daveRouter = createTRPCRouter({
                     type: input.type
                 },
             });
-            return savingAccount;
+            const transaction = await ctx.prisma.transaction.create({
+                data: {
+                    amount: 0,
+                    accountId: savingAccount.id,
+                }
+            })
+            return [savingAccount, transaction];
+        }),
+    createTransaction: protectedProcedure
+        .input(
+            z.object({
+                amount: z.number(),
+                accountId: z.string(),
+            })
+        )
+        .mutation( async ({ctx, input}) => {
+            const userId = ctx.session.user.id;
+            if (userId === undefined) {
+                return
+            }
+            const savingAccount = await ctx.prisma.savingsAccount.findFirst({
+                where: {
+                    id: input.accountId
+                }
+            })
+            if (savingAccount === null) {
+                return
+            }
+            const transaction = await ctx.prisma.transaction.create({
+                data: {
+                    amount: input.amount,
+                    accountId: savingAccount.id
+
+                }
+            })
+            return transaction
+        }),
+    getSavingsAccountSum: protectedProcedure
+        .input(z.string())
+        .query(async ({ ctx, input}) => {
+            const transactions = await ctx.prisma.transaction.findMany({
+                where: {
+                    account: {
+                        id: input
+                    }
+                }
+            })
+            return sum(transactions, 'amount');
+        }),
+    getAccountTransactions: protectedProcedure
+        .input(
+            z.string()
+        )
+        .query(async ({ctx, input}) => {
+            const transactions = await ctx.prisma.transaction.findMany({
+                where: {
+                    account: {
+                        user: {
+                            id: ctx.session.user.id
+                        },
+                        id: input
+                    }
+                }
+            })
+            return transactions
         })
 })
